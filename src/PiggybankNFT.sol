8361cb9ac118c202d758bc5094166aa3a4f82f49
nicholas <nnnnicholas@users.noreply.github.com>
2023-06-25 05:33:33 -0400

    remove cruft

--- src/PiggybankNFT.sol
+++ src/PiggybankNFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import {Base64} from "base64-sol/base64.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-// import "./MyERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "reference/src/lib/ERC6551AccountLib.sol";
 import "reference/src/interfaces/IERC6551Registry.sol";
